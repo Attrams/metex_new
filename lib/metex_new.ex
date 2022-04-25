ef3e6279901eb8dbdd8f463a59b5653e03b8c3ef
Papa Attrams <48452451+Attrams@users.noreply.github.com>
2022-04-25 04:47:54 +0000

    initial commit

---
+++ lib/metex_new.ex
@@ -0,0 +1,18 @@
+defmodule MetexNew do
+  @moduledoc """
+  Documentation for `MetexNew`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MetexNew.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/metex_new_test.exs
@@ -0,0 +1,8 @@
+defmodule MetexNewTest do
+  use ExUnit.Case
+  doctest MetexNew
+
+  test "greets the world" do
+    assert MetexNew.hello() == :world
+  end
+end
